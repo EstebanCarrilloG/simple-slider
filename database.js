3b1b8bd3f4174b77785d7465f5fe2cedb674e435
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2024-10-07 16:39:39 -0500

    Update database.js

--- database.js
+++ database.js
@@ -6,27 +6,27 @@ const sliderDb = [
   },
   {
     title:"Titulo de ejemplo dos",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://loremflickr.com/cache/resized/65535_53408697097_9b4acb9b9e_h_1000_300_nofilter.jpg"
   },
   {
     title:"Titulo de ejemplo tres",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://loremflickr.com/cache/resized/65535_53738386386_0b26bbd9ee_h_1000_300_nofilter.jpg"
   },
   {
     title:"Titulo de ejemplo cuatro",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://fastly.picsum.photos/id/733/1000/300.jpg?hmac=U4MUrZDSnXhy_r46zUxbJ4IO7zOuRj8fyYaEQ1IS5QQ"
   },
   {
     title:"Titulo de ejemplo cinco",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://loremflickr.com/cache/resized/65535_53408697097_9b4acb9b9e_h_1000_300_nofilter.jpg"
   },
   {
     title:"Titulo de ejemplo seis",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://fastly.picsum.photos/id/234/1000/300.jpg?hmac=e4sr52ojXkkuvmpDbQARWFV1kmba9F_J2DStivb9oFE"
   },
   {
     title:"Titulo de ejemplo siete",
-    src:"https://random-image-pepebigotes.vercel.app/api/random-image"
+    src:"https://loremflickr.com/cache/resized/65535_53291109557_9378aa6b7b_1000_300_nofilter.jpg"
   },
 
 ]
